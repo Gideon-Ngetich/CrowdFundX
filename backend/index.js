f72a840f00d522cc949840d4d8e264386db13d9f
Gideon-Ngetich <sunbird082003@gmail.com>
2025-04-03 13:02:53 +0300

    Cors error fixes

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,10 @@ const groupFundingStk = require('./routes/GroupFundingCallback.route')
 
 const app = express()
 
-app.use(cors())
+app.use(cors(
+  origin = process.env.FRONTEND_URL,
+  credentials = true
+))
 app.use(express.json())
 app.use(bodyParser.json())
 connectToDB()
